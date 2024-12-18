58908f87afd2efd19ad1e42aa572feb96491714f
Julnarot <rauljhonatan.tola@gmail.com>
2024-12-17 22:50:31 -0500

    test(adventjs): resolving day 01 challenges

---
+++ ts-js/adventjs/2024/challenge01.test.js
@@ -0,0 +1,12 @@
+import { test, expect } from 'vitest';
+import { prepareGifts } from './challenge01';
+
+test('example test case 01', () => {
+  expect(prepareGifts([3, 1, 2, 3, 4, 2, 5])).toEqual([1, 2, 3, 4, 5]);
+});
+test('example test case 02', () => {
+  expect(prepareGifts([6, 5, 5, 5, 5])).toEqual([5, 6]);
+});
+test('example test case 03', () => {
+  expect(prepareGifts([])).toEqual([]);
+});
