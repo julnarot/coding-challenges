dc583984897ed7e5b468b79a675fec49291e1dd6
Julnarot <rauljhonatan.tola@gmail.com>
2024-12-19 01:06:44 -0500

    test(adventjs): adding test cases for day 5 challenge

--- ts-js/adventjs/2024/challenge05.test.js
+++ ts-js/adventjs/2024/challenge05.test.js
@@ -11,3 +11,23 @@ test('Test case 01', () => {
     ];
     expect(organizeShoes(shoes)).toEqual([38, 42])
 });
+test('Test case 02', () => {
+    const shoes = [
+        { type: 'I', size: 38 },
+        { type: 'R', size: 38 },
+        { type: 'I', size: 38 },
+        { type: 'I', size: 38 },
+        { type: 'R', size: 38 }
+    ];
+    expect([38, 38]).toEqual(organizeShoes(shoes))
+});
+test('Test case 03', () => {
+    const shoes = [
+        { type: 'I', size: 38 },
+        { type: 'R', size: 36 },
+        { type: 'R', size: 42 },
+        { type: 'I', size: 41 },
+        { type: 'I', size: 43 }
+    ];
+    expect(organizeShoes(shoes)).toEqual([])
+});
