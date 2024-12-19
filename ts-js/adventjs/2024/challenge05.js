b15c4b4c87ee67693103d10ebc998d882cb0444e
Julnarot <rauljhonatan.tola@gmail.com>
2024-12-18 23:25:22 -0500

    test(adventjs): resolved day 5 challenge

---
+++ ts-js/adventjs/2024/challenge05.js
@@ -0,0 +1,11 @@
+export function organizeShoes(shoes) {
+    const shoeObj = {};
+    shoes.forEach(element => {
+        if (shoeObj.hasOwnProperty(element.size)) {
+            shoeObj[element.size] = [...shoeObj[element.size], element];
+        } else {
+            shoeObj[element.size] = [element];
+        }
+    });
+    return Object.keys(shoeObj).filter(key => [...shoeObj[key].map(shoe => shoe.type)].length > 1).map(k => Number(k))
+}
---
+++ ts-js/adventjs/2024/challenge05.test.js
@@ -0,0 +1,14 @@
+import { test, expect } from 'vitest';
+import { organizeShoes } from './challenge05'
+
+test('Test case 01', () => {
+    const shoes = [
+        { type: 'I', size: 38 },
+        { type: 'R', size: 38 },
+        { type: 'R', size: 42 },
+        { type: 'I', size: 41 },
+        { type: 'I', size: 42 }
+    ];
+    console.log(organizeShoes(shoes));
+    expect(organizeShoes(shoes)).toEqual([38, 42])
+});
