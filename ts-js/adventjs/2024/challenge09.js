b10c962ac4ea46324fef4d1a93fb0e287b6fb49e
Julnarot <rauljhonatan.tola@gmail.com>
2024-12-21 00:18:23 -0500

    fix: fixing logical for return valid value, adding test case

--- ts-js/adventjs/2024/challenge09.js
+++ ts-js/adventjs/2024/challenge09.js
@@ -1,31 +1,30 @@
 export function moveTrain(board, mov) {
-    // Code here
     let action = 'none';
     board.forEach((row, index) => {
         const posix = row.indexOf('@');
         if (posix >= 0) {
             if (mov === 'U') {
                 if (index - 1 >= 0 && index < board.length && posix >= 0 && posix < row.length) {
-                    action = board[index - 1].charAt(posix) === '*' ? 'eat' : 'none';
+                    action = board[index - 1].charAt(posix) === '*' ? 'eat' : board[index - 1].charAt(posix) === 'o' ? 'crash' : 'none';
                 } else {
                     action = 'crash';
                 }
             } else if (mov === 'R') {
-                if (index >= 0 && index < board.length && posix >= 0 && (posix+1) < row.length) {
-                    action = row.charAt(posix + 1) === '*' ? 'eat' : 'none';
+                if (index >= 0 && index < board.length && posix >= 0 && (posix + 1) < row.length) {
+                    action = row.charAt(posix + 1) === '*' ? 'eat' : row.charAt(posix + 1) === 'o' ? 'crash' : 'none';
                 } else {
                     action = 'crash';
                 }
             } else if (mov === 'L') {
                 if (index >= 0 && index < board.length && (posix - 1) >= 0 && posix < row.length) {
-                    action = row.charAt(posix - 1) === '*' ? 'eat' : 'none';
+                    action = row.charAt(posix - 1) === '*' ? 'eat' : row.charAt(posix - 1) === 'o' ? 'crash' : 'none';
                 } else {
                     action = 'crash';
                 }
-            
+
             } else if (mov === 'D') {
                 if (index >= 0 && index + 1 < board.length && posix >= 0 && posix < row.length) {
-                    action = board[index + 1].charAt(posix) === '*' ? 'eat' : board[index + 1].charAt(posix)=== 'o' ? 'crash' : 'none';
+                    action = board[index + 1].charAt(posix) === '*' ? 'eat' : board[index + 1].charAt(posix) === 'o' ? 'crash' : 'none';
                 } else {
                     action = 'crash';
                 }
--- ts-js/adventjs/2024/challenge09.test.js
+++ ts-js/adventjs/2024/challenge09.test.js
@@ -1,23 +1,124 @@
 import { test, expect } from 'vitest';
 import { moveTrain } from './challenge09';
-const board = [
-    '·····',
-    '*····',
-    '@····',
-    'o····',
-    'o····'
-];
+
 
 test('test case 01', () => {
+    const board = [
+        '·····',
+        '*····',
+        '@····',
+        'o····',
+        'o····'
+    ];
     expect(moveTrain(board, 'U')).toEqual('eat');
 });
 
 test('test case 02', () => {
+    const board = [
+        '·····',
+        '*····',
+        '@····',
+        'o····',
+        'o····'
+    ];
     expect(moveTrain(board, 'D')).toEqual('crash');
 });
+
 test('test case 03', () => {
+    const board = [
+        '·····',
+        '*····',
+        '@····',
+        'o····',
+        'o····'
+    ];
     expect(moveTrain(board, 'L')).toEqual('crash');
 });
+
 test('test case 04', () => {
+    const board = [
+        '·····',
+        '*····',
+        '@····',
+        'o····',
+        'o····'
+    ];
     expect(moveTrain(board, 'R')).toEqual('none');
+});
+
+test('test case 05', () => {
+    const board = [
+        "·····",
+        "··*··",
+        "··@··",
+        "··o··",
+        "··o··"
+    ];
+    expect(moveTrain(board, 'U')).toEqual('eat');
+});
+
+test('test case 06', () => {
+    const board = [
+        "·····",
+        "··*··",
+        "··.··",
+        "··o··",
+        "··@··"
+    ];
+    expect(moveTrain(board, 'D')).toEqual('crash');
+});
+
+test('test case 07', () => {
+    const board = [
+        "·····",
+        "··*··",
+        "··@··",
+        "··o··",
+        "··o··"
+    ];
+    expect(moveTrain(board, 'D')).toEqual('crash');
+});
+
+test('test case 08', () => {
+    const board = [
+        "·····",
+        "··*··",
+        "··@··",
+        "··o··",
+        "··o··"
+    ];
+    expect(moveTrain(board, 'R')).toEqual('none');
+});
+
+test('test case 09', () => {
+    const board = [
+        "··@··",
+        "··o··",
+        "·*o··",
+        "··o··",
+        "··o··"
+    ];
+    expect(moveTrain(board, 'U')).toEqual('crash');
+});
+
+test('test case 10', () => {
+    const board = [
+        "··@··",
+        "··o··",
+        "·*o··",
+        "··o··",
+        "··o··"
+    ];
+    expect(moveTrain(board, 'L')).toEqual('none');
+});
+
+test('test case 11', () => {
+    const board = [
+        "·····",
+        "··@··",
+        "··*··",
+        "·····",
+        "·····"
+    ];
+    expect(moveTrain(board, 'D')).toEqual('eat');
 });
