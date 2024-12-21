e0f8b9f7cf597243761e13c1039ad4acd0ff4f7c
Julnarot <rauljhonatan.tola@gmail.com>
2024-12-20 22:47:17 -0500

    test(adventjs): adding cases to validate resolution  day 8 challenge

--- ts-js/adventjs/2024/challenge08.test.js
+++ ts-js/adventjs/2024/challenge08.test.js
@@ -5,5 +5,18 @@ test('test case 01', () => {
     const expectedVal = `  ~~~~~~~~~~ /1
  ~~~~~r~~~~ /2
 ~~~~~~~r~~ /3`;
-expect(drawRace([0, 5, -3], 10)).toEqual(expectedVal);
+    expect(drawRace([0, 5, -3], 10)).toEqual(expectedVal);
+});
+test('test case 02', () => {
+    const expectedVal = `   ~~r~~~~~ /1
+  ~~~~~~~r /2
+ ~~~~~~~~ /3
+~~~~~r~~ /4`;
+    expect(drawRace([2, -1, 0, 5], 8)).toEqual(expectedVal);
+});
+test('test case 03', () => {
+    const expectedVal = `  ~~~r~~~~~~~~ /1
+ ~~~~~~~r~~~~ /2
+~~~~~~~~~~r~ /3`;
+    expect(drawRace([3, 7, -2], 12)).toEqual(expectedVal);
 });
