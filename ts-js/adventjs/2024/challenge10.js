f9fb1e9210ea02505bf112eef6c459e8770a0914
Julnarot <rauljhonatan.tola@gmail.com>
2024-12-22 13:06:31 -0500

    feat(adventjs): completed logical for solve day 10 challenge

--- ts-js/adventjs/2024/challenge10.js
+++ ts-js/adventjs/2024/challenge10.js
@@ -1,27 +1,21 @@
 export function compile(instructions) {
-    const memory = {};
+    const memory = {}; 
 
     const move = (regFrom, regTo) => {
-        memory[regTo] = memory[regFrom];
-    }
-    const copy = (registro, value) => {
-        memory[registro] = Number(value);
-    }
+        memory[regTo] = isNaN(regFrom) ? memory[regFrom] ?? 0 : Number(regFrom);
+    };
+
     const increment = (registro) => {
-        memory[registro] = !!memory[registro] ? Number(memory[registro]) + 1 : 1;
-    }
+        memory[registro] = (memory[registro] ?? 0) + 1;
+    };
+
     const decrement = (registro) => {
-        memory[registro] = !!memory[registro] ? Number(memory[registro]) - 1 : -1;
-    }
+        memory[registro] = (memory[registro] ?? 0) - 1;
+    };
 
-
-    const applyAction = (operation, instr1, instr2, value) => {
+    const applyAction = (operation, instr1, instr2) => {
         if (operation === 'MOV') {
-            if (!isNaN(instr1)) {
-                copy(instr2, instr1);
-            } else {
-                move(instr1, instr2, value);
-            }
+            move(instr1, instr2);
             return -1;
         } else if (operation === 'INC') {
             increment(instr1);
@@ -30,21 +24,20 @@ export function compile(instructions) {
             decrement(instr1);
             return -1;
         } else if (operation === 'JMP') {
-            return memory[instr1] === 0 ? instr2 : -1;
+            return (memory[instr1] ?? 0) === 0 ? Number(instr2) : -1;
         }
-    }
-    const startTime = Date.now();
+    };
+
     for (let index = 0; index < instructions.length; index++) {
-        if (Date.now() - startTime > 10) {
-            throw new Error("timeout");
-        }
         const instruction = instructions[index];
         const [operation, instr1, instr2] = instruction.split(" ");
-        const resul = applyAction(operation, instr1, instr2);
-        if (resul >= 0) {
-            index = resul - 1;
-             continue
+        const result = applyAction(operation, instr1, instr2);
+
+        if (result >= 0) {
+            index = result - 1; 
+            continue;
         }
     }
-    return memory['A'];
+
+    return memory['A']; 
 }
