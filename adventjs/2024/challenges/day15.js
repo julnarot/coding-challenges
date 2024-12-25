fa7a63467f273bd5f00c4c465829aaea84e39849
Julnarot <rauljhonatan.tola@gmail.com>
2024-12-25 01:06:32 -0500

    wip:(adventjs): initial solving for first case

--- adventjs/2024/challenges/day15.js
+++ adventjs/2024/challenges/day15.js
@@ -1,31 +1,31 @@
 export function drawTable(data) {
     let rows = [];
-
     const rowsSize = (list) => {
-        list.map(rc => {
-            Array.from({ length: rc.length }).map((_, i) => {
-                ob[i] = ob.hasOwnProperty(i) ? [...ob[i], rc[i].length] : []
+        const ob = {}
+        list.forEach(rc => {
+            Array.from({ length: rc.length }).forEach((_, i) => {
+                ob[i] = ob.hasOwnProperty(i) ? [...ob[i], rc[i].length] : [rc[i].length];
             });
+
         })
         return Object.keys(ob).map(o => {
-            const or = ob[o].sort((a, b) => a.res > b ? 1 : a < b ? -1 : 0)
+            const or = ob[o].sort((a, b) => a.res > b ? 1 : a < b ? -1 : 0);
             return or[or.length - 1];
         });
     }
     const columns = data.map(element => {
         const val = [];
         const keys = Object.keys(element);
-        rows = keys;
+        rows = [...keys].map(m => `${m.charAt(0).toUpperCase()}${m.slice(1)}`);
         keys.forEach(key => {
-            val.push(element[key])
+            val.push(`${element[key]}`)
         })
-        // console.log(val)
         return val;
     });
-    const ob = {}
-    const rowsAndColumns = [rows, ...columns];
-    const rsize = rowsSize(rowsAndColumns);
+
+    const rowsAndColumns = [[], rows, [], ...columns, []];
+    const rsize = rowsSize([rows, ...columns]);
     return rowsAndColumns.map(rc => {
-        return `| ${rc.map(r=>r).join('|')} |`
+        return rc.length > 0 ? `| ${rc.map((r, i) => '' + r.padEnd(rsize[i], ' ')).join(' | ')}  |` : `+${rsize.map(r => "".padEnd(r + 2, '-')).join("+")}-+`
     }).join('\n')
 }
