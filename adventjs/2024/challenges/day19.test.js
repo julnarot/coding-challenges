04316a9366763867c8994ee96ebb8ae5eb2f9749
Julnarot <rauljhonatan.tola@gmail.com>
2024-12-29 22:38:42 -0500

    test(adventjs): adding second test case for validate day19 solving

--- adventjs/2024/challenges/day19.test.js
+++ adventjs/2024/challenges/day19.test.js
@@ -4,4 +4,10 @@ import { distributeWeight } from "./day19.js";
 test('Unit test 01', ()=> {
     expect(distributeWeight(1)).toEqual(` _
 |_|`);
-})
+});
+
+test('Unit test 02', ()=> {
+    expect(distributeWeight(2)).toEqual(` ___
+|___|`);
+});
+
