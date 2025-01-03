738285de13e99c7e97a3afa92e51ac0b66146351
Julnarot <rauljhonatan.tola@gmail.com>
2025-01-02 22:15:26 -0500

    wip(adventjs): solution for adventjs for challenges day 21

---
+++ adventjs/2024/challenges/day21.js
@@ -0,0 +1,13 @@
+export function treeHeight(tree) {
+
+    const nodeSide = (node) => {
+        if (node === null) {
+            return null;
+        }
+        const rightQty = nodeSide(node.right);
+        const leftQty = nodeSide(node.left);
+        let maxQty = rightQty > leftQty ? rightQty : leftQty;
+        return maxQty + 1;
+    }
+    return nodeSide(tree);
+}
