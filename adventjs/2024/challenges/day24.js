0f5b8a49bce18542a437d8686b169e33a5f18351
Julnarot <rauljhonatan.tola@gmail.com>
2025-01-05 11:55:38 -0500

    feat(adventjs): day 24 challenge was solved

--- adventjs/2024/challenges/day24.js
+++ adventjs/2024/challenges/day24.js
@@ -1,17 +1,26 @@
 export function isTreesSynchronized(tree1, tree2) {
-
     const nodeSide = (node) => {
         if (node === null) {
-            return 0;
+            return null;
         }
-        const right = nodeSide(node.right ?? null);
-        const left = nodeSide(node.left ?? null);
-        // let maxQty = rightQty > leftQty ? rightQty : leftQty;
-        return [node.value, left, right];
-    }
-    const [tree1First,...tree1Rest] = nodeSide(tree1)
-    const [tree2First,...tree2Rest] = nodeSide(tree2)
-    // const s2 = nodeSide(tree2)
-    
-    return [JSON.stringify(tree1Rest) === JSON.stringify([...tree2Rest].reverse()), ...new Set([tree1First, tree2First])]
+        return {
+            value: node.value,
+            left: nodeSide(node.left ?? null),
+            right: nodeSide(node.right ?? null),
+        };
+    };
+
+    const compareNodes = (node1, node2) => {
+        if (!node1 && !node2) return true;
+        if (!node1 || !node2) return false;
+        if (node1.value !== node2.value) return false;
+        return (
+            compareNodes(node1.left, node2.right) &&
+            compareNodes(node1.right, node2.left)
+        );
+    };
+
+    const tree1Root = nodeSide(tree1);
+    const tree2Root = nodeSide(tree2);
+    return [compareNodes(tree1Root, tree2Root), tree1Root?.value];
 }
--- adventjs/2024/challenges/day24.test.js
+++ adventjs/2024/challenges/day24.test.js
@@ -16,6 +16,21 @@ test('Unit test 01', ()=> {
     expect(isTreesSynchronized(tree1, tree2)).toEqual([true, '🎄']);
 })
 
+test('Unit test 02', () => {
+  const tree1 = {
+    value: '⭐️',
+    left: { value: '🎅' },
+    right: { value: '🎁' }
+  };
+  
+  const tree2 = {
+    value: '⭐️',
+    left: { value: '🎅' },
+    right: { value: '🎁' }
+  };
+  expect(isTreesSynchronized(tree1, tree2)).toEqual([false, '⭐️']);
+});
+
 test('Unit test 06', ()=> {
     const tree1 = {
         value: '🎄'
@@ -26,3 +41,34 @@ test('Unit test 06', ()=> {
       }
     expect(isTreesSynchronized(tree1, tree2)).toEqual([false, '🎄']);
 })
+
+test('Unit test 07', () => {
+  const tree1 = {
+    value: '🎄',
+    left: { value: '🎅', left: { value: '🎄' }, right: { value: '⭐' } },
+    right: { value: '🎁' }
+  };
+
+  const tree2 = {
+    value: '🎄',
+    left: { value: '🎁' },
+    right: { value: '🎅', left: { value: '⭐' }, right: { value: '🎄' } }
+  };
+
+  expect(isTreesSynchronized(tree1, tree2)).toEqual([true, '🎄']);
+});
+
+
+test('Unit test 08', () => {
+  const tree1 = {
+    value: '🎄',
+    left: { value: '⭐' }
+  };
+
+  const tree2 = {
+    value: '🎄',
+    right: { value: '⭐' }
+  };
+
+  expect(isTreesSynchronized(tree1, tree2)).toEqual([true, '🎄']);
+});
