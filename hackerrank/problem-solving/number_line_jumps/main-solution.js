95322e1a5b38c1be5030cca402398f4222c15f8e
Julnarot <rauljhonatan.tola@gmail.com>
2025-10-06 22:18:22 -0500

    feat: add solving for exercise: number line jumps.

---
+++ hackerrank/problem-solving/number_line_jumps/main-solution.js
@@ -0,0 +1,18 @@
+export function kangaroo(x1, v1, x2, v2) {
+  let isSamePosix = false;
+  let x1NewPos = x1;
+  let x2NewPos = x2;
+  for (let index = 0; index < 10000; index++) {
+      console.log(`I: ${index}`)
+    x1NewPos = x1NewPos + v1;
+    x2NewPos = x2NewPos + v2;
+    console.log(x1NewPos)
+    console.log(x2NewPos)
+    isSamePosix = x1NewPos === x2NewPos;
+    if (isSamePosix) {
+      break;
+    }
+  }
+
+  return isSamePosix ? "YES" : "NO";
+}
---
+++ hackerrank/problem-solving/number_line_jumps/main-solution.test.js
@@ -0,0 +1,13 @@
+import { kangaroo } from "./main-solution";
+
+import { test, expect } from "vitest";
+
+test("Sample 00", () => {
+  expect(kangaroo(2,1,1,2)).toEqual("YES");
+});
+test("Test case 00", () => {
+  expect(kangaroo(0, 3, 4, 2)).toEqual("YES");
+});
+test("Test case 01", () => {
+//   expect(kangaroo(0, 2, 5, 3)).toEqual("NO");
+});
